375d6465bc649f44cca5efdf33fd6e42f04a9907
denmonzh <den8795@gmail.com>
2019-01-23 15:14:32 +0200

    Updates

---
+++ precache-manifest.8c74c0ad5beb35fcca392ac3843efbeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8de67e17233c0982a7a",
+    "url": "/personal_page/static/css/main.6b276453.chunk.css"
+  },
+  {
+    "revision": "b8de67e17233c0982a7a",
+    "url": "/personal_page/static/js/main.b8de67e1.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/personal_page/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "bee404fcdc8b5f93b089",
+    "url": "/personal_page/static/js/runtime~main.bee404fc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal_page/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3352d250145ba32dd585ff0ea9dccb2c",
+    "url": "/personal_page/index.html"
+  }
+];
